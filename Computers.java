c8f010a7e8f676bcf83aac15f1108d2b89db0b65
nrgowraram <97777027+nrgowraram@users.noreply.github.com>
2022-01-14 23:51:21 -0600

    Add files via upload

---
+++ Computers.java
@@ -0,0 +1,11 @@
+package computer;
+public abstract class Computers {
+String description = "Select computer";
+
+public String getDescription() {
+return description;
+}
+
+public abstract double cost();
+}
+
